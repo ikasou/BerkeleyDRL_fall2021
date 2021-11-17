b5d8c90272eea480764fd53f7ee5f749c1a39621
ikasou <Ilias.kasoumis@gmail.com>
2021-11-17 18:29:27 +0000

    minor fix

--- hw1/cs285/infrastructure/colab_utils.py
+++ hw1/cs285/infrastructure/colab_utils.py
@@ -1,6 +1,6 @@
 from gym.wrappers import Monitor
 import glob
-import io
+import io, os
 import base64
 from IPython.display import HTML
 from IPython import display as ipythondisplay
@@ -8,7 +8,9 @@ from IPython import display as ipythondisplay
 ## modified from https://colab.research.google.com/drive/1flu31ulJlgiRL1dnN2ir8wGh9p7Zij2t#scrollTo=TCelFzWY9MBI
 
 def show_video():
-  mp4list = glob.glob('/content/video/*.mp4')
+  home = os.path.expanduser('~')
+  ex_path = '/Documents/Python/BerkeleyDRL_fall2021/hw1'
+  mp4list = glob.glob(home + ex_path + '/content/video/*.mp4')
   if len(mp4list) > 0:
     mp4 = mp4list[0]
     video = io.open(mp4, 'r+b').read()
@@ -22,5 +24,7 @@ def show_video():
     
 
 def wrap_env(env):
-  env = Monitor(env, '/content/video', force=True)
+  home = os.path.expanduser('~')
+  ex_path = '/Documents/Python/BerkeleyDRL_fall2021/hw1'
+  env = Monitor(env, home + ex_path + '/content/video', force=True)
   return env
