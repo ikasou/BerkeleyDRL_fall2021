55c34c663887e158bcdb159e310603ca9052c18c
Ilias Kasoumis <ilias.kasoumis@gmail.com>
2022-07-08 12:53:32 +0100

    add device ops

--- hw4/cs285/infrastructure/pytorch_util.py
+++ hw4/cs285/infrastructure/pytorch_util.py
@@ -62,6 +62,9 @@ def init_gpu(use_gpu=True, gpu_id=0):
     if torch.cuda.is_available() and use_gpu:
         device = torch.device("cuda:" + str(gpu_id))
         print("Using GPU id {}".format(gpu_id))
+    elif use_gpu and ('mps' in dir(torch.backends)) and torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Using GPU via device MPS")        
     else:
         device = torch.device("cpu")
         print("GPU not detected. Defaulting to CPU.")
